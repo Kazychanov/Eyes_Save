e7d5fabdb5a84bc4d3330e9b087bebe22130ef9d
Terriuawara Maguir <terriuawara@gmail.com>
2024-12-10 14:21:13 +0500

    Добавьте файлы проекта.

---
+++ KazychanovEyesSave/AddEditPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KazychanovEyesSave
+{
+  /// <summary>
+  /// Логика взаимодействия для AddEditPage.xaml
+  /// </summary>
+  public partial class AddEditPage : Page
+  {
+    private Agent _currentAgent = new Agent();
+    public AddEditPage()
+    {
+      InitializeComponent();
+      DataContext = _currentAgent;
+    }
+
+    private void SaveBtn_Click(object sender, RoutedEventArgs e)
+    {
+      StringBuilder errors = new StringBuilder();
+
+
+      if (ComboType.SelectedItem == null)
+        errors.AppendLine("Укажите тип агента");
+      if (string.IsNullOrWhiteSpace(_currentAgent.Title))
+        errors.AppendLine("Укажите Наименование агента");
+      if (string.IsNullOrWhiteSpace(_currentAgent.Email))
+        errors.AppendLine("Укажите почту агента");
+      if (string.IsNullOrWhiteSpace(_currentAgent.Address))
+        errors.AppendLine("Укажите адресс агента");
+      if (string.IsNullOrWhiteSpace(_currentAgent.DirectorName))
+        errors.AppendLine("Укажите ФИО директора");
+      if (string.IsNullOrWhiteSpace(_currentAgent.Priority.ToString()))
+        errors.AppendLine("Укажите приоритет");
+      if (_currentAgent.Priority <= 0)
+        errors.AppendLine("Укажите положительный приоритет агента");
+      if (string.IsNullOrWhiteSpace(_currentAgent.KPP))
+        errors.AppendLine("Укажите КПП агента");
+      if (string.IsNullOrWhiteSpace(_currentAgent.INN))
+        errors.AppendLine("Укажите ИНН агента");
+      if (string.IsNullOrWhiteSpace(_currentAgent.Phone))
+        errors.AppendLine("Укажите телефон агента");
+      else
+      {
+        string ph = _currentAgent.Phone.Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", "").Replace("+", "");
+        if (((ph[1] == '9' || ph[1] == '8' || ph[1] == '4') && ph.Length != 11)
+            || (ph[1] == 3 && ph.Length != 12))
+          errors.AppendLine("Укажите правильно телефон агента");
+      }
+      if (errors.Length > 0)
+      {
+        MessageBox.Show(errors.ToString());
+        return;
+      }
+
+      if (_currentAgent.ID == 0)
+        KazychanovEyesSaveEntities.GetContext().Agent.Add(_currentAgent);
+      try
+      {
+        KazychanovEyesSaveEntities.GetContext().SaveChanges();
+        MessageBox.Show("информация сохранена");
+        Manager.MainFrame.GoBack();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message.ToString());
+      }     
+    }
+
+    private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void DeleteBtn_Click_1(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ChangePictureBtn_Click(object sender, RoutedEventArgs e)
+    {
+      OpenFileDialog myOpenFileDialog = new OpenFileDialog();
+      if (myOpenFileDialog.ShowDialog() == true)
+      {
+        _currentAgent.Logo = myOpenFileDialog.FileName;
+        LogoImage.Source = new BitmapImage(new Uri(myOpenFileDialog.FileName));
+      }
+    }
+  }
+}
---
+++ KazychanovEyesSave/AgentPage.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+
+
+namespace KazychanovEyesSave
+{
+
+
+
+  public partial class AgentPage : Page
+  {
+
+    int CountRecords;
+    int CountPage;
+    int CurrentPage = 0;
+
+    List<Agent> CurrentPageList = new List<Agent>();
+    List<Agent> TableList;
+
+    public AgentPage()
+    {
+
+
+      InitializeComponent();
+
+      var _currentAgent = KazychanovEyesSaveEntities.GetContext().Agent.ToList();
+
+      AgentListView.ItemsSource = _currentAgent;
+
+      ComboType.SelectedIndex = 0;
+      ComboSorting.SelectedIndex = 0;
+    }
+
+    public void UpdateAgents()
+    {
+      var _currentAgent = KazychanovEyesSaveEntities.GetContext().Agent.ToList();
+      _currentAgent = _currentAgent.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Email.ToLower().Contains(TBoxSearch.Text.ToLower()) || 
+      p.Phone.Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", "").Replace("+", "")
+      .Contains(TBoxSearch.Text.Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", "").Replace("+", ""))).ToList();
+
+
+      switch (ComboSorting.SelectedIndex)
+      {
+        case 1:
+          _currentAgent = _currentAgent.OrderBy(agent => agent.Title).ToList();
+          break;
+        case 2:
+          _currentAgent = _currentAgent.OrderByDescending(agent => agent.Title).ToList();
+          break;
+        case 5:
+          _currentAgent = _currentAgent.OrderBy(agent => agent.Priority).ToList();
+          break;
+        case 6:
+          _currentAgent = _currentAgent.OrderByDescending(agent => agent.Priority).ToList();
+          break;
+        default:
+          break;
+      }
+
+      switch (ComboType.SelectedIndex)
+      {
+        case 1:
+          _currentAgent = _currentAgent.Where(agent => agent.AgentTypeTitle == "МФО").ToList();
+          break;
+        case 2:
+          _currentAgent = _currentAgent.Where(agent => agent.AgentTypeTitle == "ООО").ToList();
+          break;
+        case 3:
+          _currentAgent = _currentAgent.Where(agent => agent.AgentTypeTitle == "ЗАО").ToList();
+          break;
+        case 4:
+          _currentAgent = _currentAgent.Where(agent => agent.AgentTypeTitle == "МКК").ToList();
+          break;
+        case 5:
+          _currentAgent = _currentAgent.Where(agent => agent.AgentTypeTitle == "ОАО").ToList();
+          break;
+        case 6:
+          _currentAgent = _currentAgent.Where(agent => agent.AgentTypeTitle == "ПАО").ToList();
+          break;
+        default:
+          break;
+      }
+
+
+      AgentListView.ItemsSource = _currentAgent.ToList();
+      TableList = _currentAgent;
+      ChangePage(0, 0);
+    }
+
+    private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      UpdateAgents();
+    }
+
+    private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      UpdateAgents();
+    }
+
+    private void ComboSorting_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      UpdateAgents();
+    }
+
+    private void ChangePage(int direction, int? selectedPage)
+    {
+      CurrentPageList.Clear();
+      CountRecords = TableList.Count;
+      CountPage = (CountRecords + 9) / 10;  // Сократили формулу вычисления числа страниц
+
+      if (selectedPage.HasValue && selectedPage >= 0 && selectedPage < CountPage)
+      {
+        CurrentPage = selectedPage.Value;
+      }
+      else
+      {
+        if (direction == 1 && CurrentPage > 0)
+          CurrentPage--;
+        else if (direction == 2 && CurrentPage < CountPage - 1)
+          CurrentPage++;
+        else
+          return; // Нет изменений, можно выйти
+      }
+
+      int startIndex = CurrentPage * 10;
+      int endIndex = Math.Min(startIndex + 10, CountRecords);
+
+      for (int i = startIndex; i < endIndex; i++)
+      {
+        CurrentPageList.Add(TableList[i]);
+      }
+
+      // Обновление графического интерфейса
+      PageListBox.Items.Clear();
+      for (int i = 0; i < CountPage; i++)
+      {
+        PageListBox.Items.Add(i + 1);
+      }
+      PageListBox.SelectedIndex = CurrentPage;
+
+      TBCount.Text = endIndex.ToString();
+      TBAllRecords.Text = $" из {CountRecords}";
+
+      AgentListView.ItemsSource = CurrentPageList;
+      AgentListView.Items.Refresh();
+    }
+
+
+
+    private void ListBoxPage_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+      ChangePage(0,Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
+    }
+    private void BtnLeftDir_Click(object sender, RoutedEventArgs e)
+    {
+      ChangePage(1, null);
+    }
+
+    private void BtnRightDir_Click(object sender, RoutedEventArgs e)
+    {
+      ChangePage(2, null);
+    }
+
+    private void BtnEdit_Click(object sender, RoutedEventArgs e)
+    {
+      Manager.MainFrame.Navigate(new AddEditPage());
+    }
+
+    private void BtnAdd_Click(object sender, RoutedEventArgs e)
+    {
+      Manager.MainFrame.Navigate(new AddEditPage());
+    }
+  }
+}
